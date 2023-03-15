ecb61b5ffadc00a3da397bcb06b7e47bd2cc684e
Çağrı Şentürk <csenturk11@hotmail.com>
2023-03-15 14:18:54 +0300

    QrCodeReader  update

--- src/components/QrCodeReader/QrCodeReader.jsx
+++ src/components/QrCodeReader/QrCodeReader.jsx
@@ -1,19 +1,24 @@
 import React, { useState } from 'react';
 import { QrReader } from 'react-qr-reader';
 import { Card, Alert } from 'react-bootstrap';
-
+import getDocument from '../../api/getDocument';
 const QrCodeReader = ({ buildingCodeInput, setBuildingCodeInput }) => {
     const [data, setData] = useState(buildingCodeInput);
-
+    
     return (
         <>
             <Card>
                 <QrReader
                     onResult={(result, error) => {
                         if (!!result) {
-                            setData(result.text);
-                            setBuildingCodeInput(result.text);
-                            console.log(result);
+                            setTimeout(() => {
+                                debugger;
+                                setData(result.text);
+                                setBuildingCodeInput(result.text);
+                                console.log(result.text);
+                                var x =  getDocument(result.text);
+                                console.log(x); 
+                            }, 100);
                         }
 
                         if (error) {
@@ -22,7 +27,9 @@ const QrCodeReader = ({ buildingCodeInput, setBuildingCodeInput }) => {
                     }}
                 />
                 {data && (
-                    <Alert variant="secondary">QR Kod Sonucu: {data}</Alert>
+                    <Alert variant="secondary">QR Kod Sonucu: {data}
+                    
+                    </Alert>
                 )}
             </Card>
         </>
